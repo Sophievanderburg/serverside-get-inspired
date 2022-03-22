af57b6fe7309d6f8d4bab4a685b2b62d4bb7dbd7
Sophie van der Burg <sophievdburg9@gmail.com>
2022-03-22 13:31:06 +0100

    render quotes via node

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express')
-const request = require('request')
+const fetch = require('node-fetch')
 
 const app = express()
-const port = 3000
+const port = 4567
 
 // Link the templating engine to the express app
 app.set('view engine', 'ejs');
 
 // Tell the views engine/ejs where the template files are stored (Settingname, value)
-app.set('views', 'views');
-
+app.set('views', './views');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -17,9 +16,30 @@ app.use(express.static(__dirname + '/public'));
 app.get("/", renderPagina)
 
 function renderPagina (req, res){
-  res.render('index')
+  fetch('https://quote.api.fdnd.nl/v1/quote')
+  .then(function(response){
+    return response.json()
+  })
+  .then((jsonData) =>{
+    res.render('index', {
+      data: jsonData.data,
+      pageTitle: "Quotes"
+    })
+  })
 }
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .then((body) => body.data)
+    .catch((error) => error)
+}
